52e8981721e943012c118ddb96f06a429c2bc7f9
sonsungjong <tbxmtbfm@naver.com>
2024-06-23 05:08:51 +0900

    글쓰기 및 글수정 접근권한 추가

--- src/app/edit/[slug]/page.js
+++ src/app/edit/[slug]/page.js
@@ -1,27 +1,44 @@
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { connectDB } from "@/util/db"
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
 
 export default async function Edit({params})
 {
     console.log(params)
-    const db = (await connectDB).db('mydb');
-    let result = await db.collection('post').findOne({_id:ObjectId.createFromHexString(params.slug)})
-    //console.log(result)
+    let session = await getServerSession(authOptions);
+    
+    if(session){
+        const db = (await connectDB).db('mydb');
+        let result = await db.collection('post').findOne({_id:ObjectId.createFromHexString(params.slug)})
+        console.log(result)
+    
+        if(session.user?.email === result.email){
+            // _id 필드를 문자열로 변환해서 사용할 것
+            const resultIdString = result._id.toString();
+        
+            return(
+                <div className="p-20">
+                    <h4>수정페이지</h4>
+                    <form action="/api/post/edit" method="POST">
+                        <input type="hidden" name="id" value={resultIdString} />
+                        <input name="title" placeholder="글제목" defaultValue={result.title}/>
+                        <input name="content" placeholder="글내용" defaultValue={result.content}/>
+                        <button type="submit">수정하기</button>
+                    </form>
+                </div>
+            )
+        }else{
+            return(
+                <div>글수정은 작성자만 가능합니다</div>
+            )
+        }
+    }else{
+        return(
+            <div>로그인이 필요해요</div>
+        )
+    }
 
-    // _id 필드를 문자열로 변환해서 사용할 것
-    const resultIdString = result._id.toString();
-
-    return(
-        <div className="p-20">
-            <h4>수정페이지</h4>
-            <form action="/api/post/edit" method="POST">
-                <input type="hidden" name="id" value={resultIdString} />
-                <input name="title" placeholder="글제목" defaultValue={result.title}/>
-                <input name="content" placeholder="글내용" defaultValue={result.content}/>
-                <button type="submit">수정하기</button>
-            </form>
-        </div>
-    )
 }
 
-// 수정하기 버튼을 누르면 actino에 해당하는 URL에 요청을 보내게 되어있음
+// 수정하기 버튼을 누르면 action에 해당하는 URL에 요청을 보내게 되어있음
--- src/app/write/page.js
+++ src/app/write/page.js
@@ -1,19 +1,33 @@
-export default function Write(){
-    return(
-        <div className="p-20">
-            <h4>글작성</h4>
-            <form action="/api/post/new" method="POST">
-                <input name="title" placeholder="글제목"/>
-                <input name="content" placeholder="글내용"/>
-                <button type="submit">ACTION에 해당하는 URL에 POST요청</button>
-            </form>
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import { getServerSession } from "next-auth"
 
-            <br/>
-            <form action="/api/test" method="GET">
-                <button type="submit">ACTION에 해당하는 URL에 GET요청</button>
-            </form>
-        </div>
-    )
+export default async function Write(){
+
+    let session = await getServerSession(authOptions);
+    console.log(session);
+    
+    if(session){
+        return(
+            <div className="p-20">
+                <h4>글작성</h4>
+                <form action="/api/post/new" method="POST">
+                    <input name="title" placeholder="글제목"/>
+                    <input name="content" placeholder="글내용"/>
+                    <button type="submit">ACTION에 해당하는 URL에 POST요청</button>
+                </form>
+    
+                <br/>
+                <form action="/api/test" method="GET">
+                    <button type="submit">ACTION에 해당하는 URL에 GET요청</button>
+                </form>
+            </div>
+        )
+    }else{
+        return(
+            <div>로그인이 필요해요</div>
+        )
+    }
+
 }
 
 // title과 content 입력하게하고 DB추가
